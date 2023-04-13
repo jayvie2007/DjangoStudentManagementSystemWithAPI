8310c60216023e6fac336637083d2fe64bafe5a7
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-13 15:45:45 +0800

    added email and username validation

--- base/auth_user/admin.py
+++ base/auth_user/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import AccountModel
 
 # Register your models here.
+admin.site.register(AccountModel)
--- base/auth_user/views.py
+++ base/auth_user/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.response import Response
 
 from .models import AccountModel
@@ -20,24 +19,39 @@ class getUser(APIView):
 
 class registerUser(APIView):
     def post(self,request):
-        #emailInput = request.data['email']
-        #userInput = request.data['username']
+        errors = {}
+        emailInput = request.data['email']
+        userInput = request.data['username']
         passwordInput = request.data['password']
         passwordInput2 = request.data['password2']
+        
+        try:
+            email_exist = AccountModel.objects.get(
+                email = emailInput,
+            )
+            print(email_exist)
+            message=(f"the email {emailInput} is already taken!")
+            return Response(data={"message":message})
+        except:
+            pass
 
-        serializers = UserSerializer(data=request.data)
-
+        try:
+            users = AccountModel.objects.get(
+            username = userInput
+            )
+            message=(f"the username {userInput} is already taken!")
+            return Response(data={"message":message})
+        except:
+            pass
         if passwordInput != passwordInput2:
             message = ("Password does not match!")
             return Response(data={"status":bad_request, "message": message})      
-
         uid = generate_uid()
         request.data._mutable=True
         request.data['uid'] = uid
         request.data._mutable=False
-        
+        serializers = UserSerializer(data=request.data)
         if serializers.is_valid():
-            #uncomment the serializers.save() if you want to save the data
             #serializers.save()
             message = ("You have successfully registered")
             return Response(data={"status": created, "message": message, "Users":serializers.data})
