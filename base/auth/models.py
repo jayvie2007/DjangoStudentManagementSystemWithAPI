cc46fbd1dcd324bdc5b6f8c7a6c488ccea325899
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-13 09:57:03 +0800

    added models

--- base/auth/models.py
+++ base/auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class AccountModel(models.Model):
+    uid = models.CharField(max_length=8)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+        
