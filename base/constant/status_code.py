d8aed10e80d46c6f29ee7084360252d47a5027a4
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-14 09:39:51 +0800

    added more message

--- base/constant/status_code.py
+++ base/constant/status_code.py
@@ -1,10 +1,16 @@
 from rest_framework import status
 
+############STATUS###############
+
 ok = status.HTTP_200_OK
 not_found = status.HTTP_404_NOT_FOUND
 bad_request = status.HTTP_400_BAD_REQUEST
 created = status.HTTP_201_CREATED
 
-
+############ERRORS######################
 data_exists = ['the data currently exists']
 field_required_error = ['This field is required']
+
+###############SUCCESS##################
+updated = ['Success Updating']
+register_success = ['Successfully Registered']
