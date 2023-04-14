4ab837e5da06f250f426a5f2ab0b20a337393f53
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-04-14 09:58:25 +0800

    added edit user url

--- base/auth_user/urls.py
+++ base/auth_user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/user', views.getUser.as_view(), name="getuser"),
     path('api/usercreate', views.registerUser.as_view(), name="registeruser"),
-]
+    path('api/user/<str:uid>', views.editUser.as_view(), name="edituser"),
+   ]
