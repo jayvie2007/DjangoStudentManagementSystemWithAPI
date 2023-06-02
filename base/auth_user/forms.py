bb4d1f52fb6d0b214b4374dad73b0260e2f0fca9
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-06-02 15:57:37 +0800

    added gender, updated forms

--- base/auth_user/forms.py
+++ base/auth_user/forms.py
@@ -2,24 +2,29 @@ from django import forms
 from .models import UserData
 
 class UserForm(forms.ModelForm):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Jayvie"}))
+    middle_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Ferrer"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "De Leon"}))
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    year = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "4"}))
+    course = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "BSIT"}))
+    semester = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Second"}))
+
     class Meta:
         model = UserData
-        fields = ['fname', 'mname', 'lname', 'year', 'course', 'semester']
+        fields = ['first_name', 'middle_name', 'last_name', 'gender', 'year', 'course', 'semester']
         labels = {
-            #'student_number': 'Student Number', 
-            'fname': 'First Name', 
-            'mname': 'Middle Name', 
-            'lname': 'Last Name', 
+            'first_name': 'First Name', 
+            'middle_name': 'Middle Name', 
+            'last_name': 'Last Name', 
+            'gender': 'Gender', 
             'year': 'Year',
             'course': 'Course',
             'semester': 'Semester',
-        } 
-        widgets = {
-            #'student_number': forms.TextInput(attrs={'class': 'form-control'}), 
-            'fname': forms.TextInput(attrs={'class': 'form-control'}), 
-            'mname': forms.TextInput(attrs={'class': 'form-control'}),
-            'lname': forms.TextInput(attrs={'class': 'form-control'}), 
-            'year': forms.TextInput(attrs={'class': 'form-control'}),
-            'course': forms.TextInput(attrs={'class': 'form-control'}), 
-            'semester': forms.TextInput(attrs={'class': 'form-control'}),
         }
--- base/auth_user/models.py
+++ base/auth_user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.conf import settings
+import random
 # Create your models here.
     
 class CustomUser(AbstractUser):
@@ -15,22 +16,25 @@ class CustomUser(AbstractUser):
 
 
 class UserData(models.Model):
+    gender_choices = (
+    ('Male', "Male"),
+    ('Female', "Female"),
+    ('Other', "Other"),
+)
     student_number = models.CharField(max_length=8, default="")
-    fname = models.CharField(max_length=50)
-    mname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.CharField(choices=gender_choices,max_length=20, default="")
     year = models.CharField(max_length=20)
     course = models.CharField(max_length=50)
     semester = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.fname + " " + self.lname
-    
-    #     APPOINTMENT_STATUS = (
-    #     ('pending', "Pending"),
-    #     ('approved', "Approved"),
-    #     ('cancelled', "Cancelled"),
-    #     ('completed', "Completed"),
-    # )
+    def save(self):
+        self.student_number = str(random.randint(10000000, 99999999))
+        super(UserData, self).save()
 
-    # add choices = APPOINTMENT_STATUS to dispaly dragdown
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+
